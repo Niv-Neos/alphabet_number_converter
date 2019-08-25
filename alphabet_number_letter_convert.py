0c293437d3ae1088f517e054f8b7e3771cb38429
Niv-Neos <54379294+Niv-Neos@users.noreply.github.com>
2019-08-25 19:49:17 -0400

    Add files via upload

---
+++ alphabet_number_letter_convert.py
@@ -0,0 +1,93 @@
+alist = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+         "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+print("1. num to letter")
+print("2. letter to num")
+ui = input()
+if ui == "1":
+    print("place number")
+    ui = input()
+    print(alist[int(ui)-1])
+    input()
+
+else:
+    print("place letter")
+    ui = input()
+    if ui == "a":
+        print ("1")
+        input()
+    elif ui == "b":
+        print ("2")
+        input()
+    elif ui == "c":
+        print ("3")
+        input()
+    elif ui == "d":
+        print ("4")
+        input()
+    elif ui == "e":
+        print ("5")
+        input()
+    elif ui == "f":
+        print ("6")
+        input()
+    elif ui == "g":
+        print ("7")
+        input()
+    elif ui == "h":
+        print ("8")
+        input()
+    elif ui == "i":
+        print ("9")
+        input()
+    elif ui == "j":
+        print ("10")
+        input()
+    elif ui == "k":
+        print ("11")
+        input()
+    elif ui == "l":
+        print ("12")
+        input()
+    elif ui == "m":
+        print ("13")
+        input()
+    elif ui == "n":
+        print ("14")
+        input()
+    elif ui == "o":
+        print ("15")
+        input()
+    elif ui == "p":
+        print ("16")
+        input()
+    elif ui == "q":
+        print ("17")
+        input()
+    elif ui == "r":
+        print ("18")
+        input()
+    elif ui == "s":
+        print ("19")
+        input()
+    elif ui == "t":
+        print ("20")
+        input()
+    elif ui == "u":
+        print ("21")
+        input()
+    elif ui == "v":
+        print ("22")
+        input()
+    elif ui == "w":
+        print ("23")
+        input()
+    elif ui == "x":
+        print ("24")
+        input()
+    elif ui == "y":
+        print ("25")
+        input()
+    elif ui == "z":
+        print ("26")
+        input()
